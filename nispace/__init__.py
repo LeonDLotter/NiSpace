b8f651b1e0e41c838504e2d9a60a5948d4731da4
LeonDLotter <leondlotter@gmail.com>
2024-10-14 14:29:25 +0200

    adjust inits

--- nispace/__init__.py
+++ nispace/__init__.py
@@ -3,7 +3,11 @@
 from .utils.utils import _init_lgr
 lgr = _init_lgr(__name__)
 
-# make NiSpace available from package level
-from .api import NiSpace
+# get version
 from . import _version
 __version__ = _version.get_versions()['version']
+
+# Public API
+from .api import NiSpace
+from .workflows import simple_colocalization, simple_xsea, group_comparison
+from .datasets import fetch_reference, fetch_template, fetch_parcellation, fetch_metadata
