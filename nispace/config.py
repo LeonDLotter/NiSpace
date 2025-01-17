4b923369982eb09113865d081d77fa49f49b3eb0
LeonDLotter <leondlotter@gmail.com>
2025-01-17 11:58:32 +0100

    add schaefer fsLR

--- nispace/config.py
+++ nispace/config.py
@@ -1,7 +1,7 @@
 # Data repositories
 # public
 DATA_REPO = "LeonDLotter/NiSpace-data"
-DATA_REPO_COMMIT = "383662c8c162626b2a613e68318c30b49e1f04e2"
+DATA_REPO_COMMIT = "d897d0e28cb382876a31f58b896212d1fd1da15e"
 # private
 DATA_REPO_PRIVATE = "LeonDLotter/NiSpace-data-private"
 DATA_REPO_PRIVATE_COMMIT = "feff87dfff73bac61ec08c19e87394524210c7c4"
