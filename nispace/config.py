e7f9e67248e83f071a125d1002a55655895c5892
LeonDLotter <leondlotter@gmail.com>
2025-01-17 18:49:44 +0100

    fix pet collection

--- nispace/config.py
+++ nispace/config.py
@@ -1,7 +1,7 @@
 # Data repositories
 # public
 DATA_REPO = "LeonDLotter/NiSpace-data"
-DATA_REPO_COMMIT = "d897d0e28cb382876a31f58b896212d1fd1da15e"
+DATA_REPO_COMMIT = "6fedcd54ec66a7da949673144762262883129d59"
 # private
 DATA_REPO_PRIVATE = "LeonDLotter/NiSpace-data-private"
 DATA_REPO_PRIVATE_COMMIT = "feff87dfff73bac61ec08c19e87394524210c7c4"
