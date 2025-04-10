f7e4b6207adb363ae90c1b6ed4be5e9f8ffaf482
LeonDLotter <leondlotter@gmail.com>
2025-04-10 17:09:12 +0200

    adjust gene epxression data

--- nispace/config.py
+++ nispace/config.py
@@ -1,7 +1,7 @@
 # Data repositories
 # public
 DATA_REPO = "LeonDLotter/NiSpace-data"
-DATA_REPO_COMMIT = "bb2a3e56b39bba1a6c72439ca0cdc8bf16ecac11"
+DATA_REPO_COMMIT = "a3ae4bb359c35bb40307ffbe35106ec553340dde"
 # private
 DATA_REPO_PRIVATE = "LeonDLotter/NiSpace-data-private"
 DATA_REPO_PRIVATE_COMMIT = "feff87dfff73bac61ec08c19e87394524210c7c4"
