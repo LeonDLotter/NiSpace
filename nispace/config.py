08c375574efe6fe48b6f69ab393f82488c2d7641
LeonDLotter <leondlotter@gmail.com>
2025-04-10 20:11:35 +0200

    dataset changes: mask of pet data to whole-brain

--- nispace/config.py
+++ nispace/config.py
@@ -1,8 +1,8 @@
 # Data repositories
 # public
 DATA_REPO = "LeonDLotter/NiSpace-data"
-DATA_REPO_COMMIT = "a3ae4bb359c35bb40307ffbe35106ec553340dde"
+DATA_REPO_COMMIT = "6f0d44d48469e40cbf22f4de5bf5070c07928182"
 # private
 DATA_REPO_PRIVATE = "LeonDLotter/NiSpace-data-private"
-DATA_REPO_PRIVATE_COMMIT = "feff87dfff73bac61ec08c19e87394524210c7c4"
+DATA_REPO_PRIVATE_COMMIT = "f65f1c3ec6cbd6c2a359a420f37f5e609923a90b"
 
