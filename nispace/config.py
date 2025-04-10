306927648795faa152bd34dfbf6755f2d4f0a193
LeonDLotter <leondlotter@gmail.com>
2025-04-10 16:17:49 +0200

    add volumetric FreeSurfer parcellations

--- nispace/config.py
+++ nispace/config.py
@@ -1,7 +1,7 @@
 # Data repositories
 # public
 DATA_REPO = "LeonDLotter/NiSpace-data"
-DATA_REPO_COMMIT = "2970a45639b638880cdd46b45d8a6da763cd7926"
+DATA_REPO_COMMIT = "bb2a3e56b39bba1a6c72439ca0cdc8bf16ecac11"
 # private
 DATA_REPO_PRIVATE = "LeonDLotter/NiSpace-data-private"
 DATA_REPO_PRIVATE_COMMIT = "feff87dfff73bac61ec08c19e87394524210c7c4"
