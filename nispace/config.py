f3162491debf38eb7bd7212d469a40f51926722e
LeonDLotter <leondlotter@gmail.com>
2025-05-26 18:50:04 +0200

    adjusted neurosynth collection

--- nispace/config.py
+++ nispace/config.py
@@ -1,7 +1,7 @@
 # Data repositories
 # public
 DATA_REPO = "LeonDLotter/NiSpace-data"
-DATA_REPO_COMMIT = "1a29855201a192b36d1b04117e9f5e02ab591f99"
+DATA_REPO_COMMIT = "43087db843e43a703c3bcfc369c7aa808d7b5180"
 # private
 DATA_REPO_PRIVATE = "LeonDLotter/NiSpace-data-private"
 DATA_REPO_PRIVATE_COMMIT = "f65f1c3ec6cbd6c2a359a420f37f5e609923a90b"
