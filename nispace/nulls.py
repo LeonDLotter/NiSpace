5483bfb1de91ea738e8290b5dd896c90fec82fcf
LeonDLotter <leondlotter@gmail.com>
2025-05-30 14:41:44 +0200

    fix import error

--- nispace/nulls.py
+++ nispace/nulls.py
@@ -17,7 +17,7 @@ from numba import njit
 # import MoranRandomization function, copied from brainspace, as our default null model
 # brainspace was removed as an dependency because it installs vtk, which is a large 3d rendering
 # library that NiSpace does not use. 
-from modules.brainspace_moran import MoranRandomization
+from .modules.brainspace_moran import MoranRandomization
     
 # brainsmash is optional dependency. Moran
 try:
