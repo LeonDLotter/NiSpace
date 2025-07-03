065e620bb3528db72e7e64986cfac2967b279e72
LeonDLotter <leondlotter@gmail.com>
2025-07-03 09:59:23 +0200

    add tpm dataset

--- nispace/config.py
+++ nispace/config.py
@@ -1,7 +1,7 @@
 # Data repositories
 # public
 DATA_REPO = "LeonDLotter/NiSpace-data"
-DATA_REPO_COMMIT = "cedbc2ae6d169c75f00bffc91014cf7e99625642"
+DATA_REPO_COMMIT = "cb6b92db7eccbf386a036c3d09c4e6beb79e9afe"
 # private
 DATA_REPO_PRIVATE = "LeonDLotter/NiSpace-data-private"
 DATA_REPO_PRIVATE_COMMIT = "f65f1c3ec6cbd6c2a359a420f37f5e609923a90b"
