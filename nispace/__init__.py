3262d5d5fa944746bc0076eb69fa41fde75cc937
Leon Lotter <leondlotter@gmail.com>
2025-11-20 13:58:58 +0100

    Fix parc idc (#31)
    * fix indexing error during empty parcel handling in parcellation func
    
    * Update neuromaps requirement and suppress FutureWarnings from neuromaps/nilearn. Limit nilearn requirement to <0.13.0 (change in future!). Avoid nilearn warnings from nulls.py. Fix indexing in parcellate.py.

--- nispace/__init__.py
+++ nispace/__init__.py
@@ -6,7 +6,6 @@ lgr = _init_lgr(__name__)
 # get version
 from . import _version
 __version__ = _version.get_versions()['version']
-
 # git commit
 def get_commit(get_version=True):
     if not get_version:
@@ -20,6 +19,14 @@ def get_commit(get_version=True):
     return commit
 __commit__ = get_commit()
 
+# hide nilearn deprecation warnings
+# TODO: update to nilearn 0.13.0, will require dropping support for python 3.9
+import os
+import warnings
+# Allow opt-in via environment variable
+if not os.environ.get("NISPACE_SHOW_FUTURE_WARNINGS"):
+    warnings.filterwarnings("ignore", category=FutureWarning, module=r"^neuromaps(\.|$)")
+    
 # Public API
 from .api import NiSpace
 from .workflows import simple_colocalization, simple_xsea, group_comparison
