facb4b78f0a9aa7380421b13b72d3d017269ce15
LeonDLotter <leondlotter@gmail.com>
2025-11-21 13:03:52 +0100

    add HO atlas, add RSN, update database

--- nispace/config.py
+++ nispace/config.py
@@ -1,7 +1,7 @@
 # Data repositories
 # public
 DATA_REPO = "LeonDLotter/NiSpace-data"
-DATA_REPO_COMMIT = "dc826ffb1f13b27c27591b2788be8bb5e18ec132"
+DATA_REPO_COMMIT = "3a5d6cb63d5979995d1709d552983dcab7426d52"
 # private
 DATA_REPO_PRIVATE = "LeonDLotter/NiSpace-data-private"
 DATA_REPO_PRIVATE_COMMIT = "f65f1c3ec6cbd6c2a359a420f37f5e609923a90b"
