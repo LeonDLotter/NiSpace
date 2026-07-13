6a3702fbf73a48aa354619c25f8374f556494297
LeonDLotter <leondlotter@gmail.com>
2026-07-13 15:55:35 +0200

    update intro notebooks, add ASD gene sets

--- nispace/config.py
+++ nispace/config.py
@@ -1,7 +1,7 @@
 # Data repositories
 # public
 DATA_REPO = "LeonDLotter/NiSpace-data"
-DATA_REPO_COMMIT = "8e6642cc76f433248ae77a246c319bb645673e3f"
+DATA_REPO_COMMIT = "bb3a2ab1192d7b7fdae516bc088fffb6213fc135"
 # private
 DATA_REPO_PRIVATE = "LeonDLotter/NiSpace-data-private"
 DATA_REPO_PRIVATE_COMMIT = "f65f1c3ec6cbd6c2a359a420f37f5e609923a90b"
