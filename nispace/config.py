6e7d89ee960cc837a67dbfb28aa4966798b6329e
LeonDLotter <leondlotter@gmail.com>
2026-07-13 19:00:54 +0200

    update ASDModules gene set

--- nispace/config.py
+++ nispace/config.py
@@ -1,7 +1,7 @@
 # Data repositories
 # public
 DATA_REPO = "LeonDLotter/NiSpace-data"
-DATA_REPO_COMMIT = "bb3a2ab1192d7b7fdae516bc088fffb6213fc135"
+DATA_REPO_COMMIT = "0146b7c791916065655ef931cb6ec575ea7b9ed9"
 # private
 DATA_REPO_PRIVATE = "LeonDLotter/NiSpace-data-private"
 DATA_REPO_PRIVATE_COMMIT = "f65f1c3ec6cbd6c2a359a420f37f5e609923a90b"
