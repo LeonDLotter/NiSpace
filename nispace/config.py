6994e56ffdc09866d9740d5e257244b111c3ca1e
LeonDLotter <leondlotter@gmail.com>
2026-07-31 08:59:57 +0200

    dataset update

--- nispace/config.py
+++ nispace/config.py
@@ -1,7 +1,7 @@
 # Data repositories
 # public
 DATA_REPO = "LeonDLotter/NiSpace-data"
-DATA_REPO_COMMIT = "dd2897a4c30cd4344dd3a1ebab669ffb65777b27"
+DATA_REPO_COMMIT = "caa534655a43ba6cdafe4377144b77b55c573768"
 # private
 DATA_REPO_PRIVATE = "LeonDLotter/NiSpace-data-private"
 DATA_REPO_PRIVATE_COMMIT = "f65f1c3ec6cbd6c2a359a420f37f5e609923a90b"
